0e3c9b8f013223b598ac0321b69c94b6e325310d
Michael Watson <michael@projectsimply.com>
2022-05-16 13:24:24 +0100

    Create book-a-demo.php

---
+++ wp-content/themes/plot-custom/templates/pages/book-a-demo.php
@@ -0,0 +1,10 @@
+<?php plotGetTemplatePart('parts/header') ?>
+
+<?php plotGetTemplatePart('parts/banner') ?>
+
+<!-- Start of Meetings Embed Script -->
+    <div class="meetings-iframe-container" data-src="https://meetings.hubspot.com/christian34?embed=true"></div>
+    <script type="text/javascript" src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"></script>
+  <!-- End of Meetings Embed Script -->
+
+<?php plotGetTemplatePart('parts/footer') ?>
